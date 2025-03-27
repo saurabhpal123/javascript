b30ba33c48a645a6784f4770eb04ccdb3695aec9
Saurabh Pal <148249331+saurabhpal123@users.noreply.github.com>
2025-03-27 15:31:03 +0000

    string completed

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,40 @@
+const name = "saurabh"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+//console.log(`hello my name id ${name} and my reo name is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt('4'));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+//const anotherString = gameName.slice(-8, 4)
+//const anotherString = gameName.slice(-2, 3)
+//console.log(anotherString);
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://saurabh.com/saurabh%20suryavanshi"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('saurabh'));
+
+
+
+
+
+
